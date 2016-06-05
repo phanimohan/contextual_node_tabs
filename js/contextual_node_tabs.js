09eadea56c4bf51bcdd3ed4133f96f2fc4f9917f
Phani Mohan. Y <phanimohan.yarlagadda@cognizant.com>
2016-06-05 09:23:35 +0530

    Front end implementation.

--- js/contextual_node_tabs.js
+++ js/contextual_node_tabs.js
@@ -3,10 +3,37 @@
  * Attaches behavior for the Contextual Node Tabs module.
  */
 
-(function ($, _, Backbone, Drupal, drupalSettings) {
+(function ($, Drupal, drupalSettings) {
 
   'use strict';
 
-  alert('From JS');
+  var $dropdown = $('div.dropdownWrapper'),
+      $drpBtn   = $dropdown.find('div.dropdownLabel');
 
-})(jQuery, _, Backbone, Drupal, drupalSettings);
+  $drpBtn.on('click', function(e){
+    e.stopPropagation();
+    var $element = $(this).parent();
+    $element.find('.dropdownPanel').fadeToggle(200);
+  });
+	
+	$(".account").click(function() {
+		$(".submenu").toggleClass('menu-show');
+	});
+	
+	//Mouse click on sub menu
+	$(".submenu").mouseup(function() {
+		return false;
+	});
+
+	//Mouse click on my account link
+	$(".account").mouseup(function() {
+		return false;
+	});
+
+  $("document").mouseup(function(){
+		$(".submenu").hide();
+		$(".account").attr('id', '');
+    $('.dropdownPanel').hide(200);
+  });
+
+})(jQuery, Drupal, drupalSettings);
